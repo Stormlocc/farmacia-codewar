d3277418ae901bde891d0a274ea2300506810ce0
Anthony Mayron Lopez <anthonymlocc@hotmail.com>
2023-03-09 16:18:32 -0500

    cambiar pass - enviar submit

--- vista/editar_datos_personales.php
+++ vista/editar_datos_personales.php
@@ -8,6 +8,55 @@ if ($_SESSION['us_tipo'] == 1) {
     <?php
     include_once 'layouts/nav.php';
     ?>
+
+    <!-- Modal -->
+    <div class="modal fade" id="cambioContra" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
+                <div class="modal-body">
+                    <div class="text-center">
+                        <img src="../img/avatar.png" class="profile-user-img img-fluid img-circle">
+                    </div>
+                    <div class="text-center">
+                        <b>
+                            <?php
+                                echo $_SESSION['nombre_us']
+                            ?>                         
+                        </b>
+                    </div>
+                    <form id="form-pass">
+                        <div class="input-group mb-3">   
+                            <div class="input-group-prepend">
+                                <spam class="input-group-text">
+                                    <i class="fas fa-unlock"></i>
+                                </spam>    
+                            </div>
+                            <input id="oldpass" type="password" class="form-control" placeholder="Ingrese password actual">
+                        </div>
+                        <div class="input-group mb-3">   
+                            <div class="input-group-prepend">
+                                <spam class="input-group-text">
+                                    <i class="fas fa-lock"></i>
+                                </spam>    
+                            </div>
+                            <input id="newpass" type="text" class="form-control" placeholder="Ingrese nueva contraseña">
+                        </div>
+                    </form>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-outline-secondary" data-dismiss="modal">Cerrar</button>
+                    <button type="submit" class="btn bg-gradient-primary">Guardar</button>
+                </div>
+            </div>
+        </div>
+    </div>
+
     <!-- Content Wrapper. Contains page content -->
     <div class="content-wrapper">
         <!-- Content Header (Page header) -->
@@ -34,26 +83,27 @@ if ($_SESSION['us_tipo'] == 1) {
                         <div class="col-md-3">
                             <div class="card card-success card-outline">
                                 <!--En la sgte linea se recupera el usuario en una variable para el javascrip-->
-                                <input id="id_usuario" type="hidden" value="<?php echo $_SESSION['usuario']?>">
+                                <input id="id_usuario" type="hidden" value="<?php echo $_SESSION['usuario'] ?>">
                                 <div class="card-body box-profile">
                                     <div class="text-center">
                                         <img src="../img/avatar.png" class="profile-user-img img-fluid img-circle">
-                                        <h3 id= "nombre_us" class="profile-username text-center text-success">Nombre</h3>
-                                        <p id= "apellidos_us" class="text-muted text-center">Apellidos</p>
+                                        <h3 id="nombre_us" class="profile-username text-center text-success">Nombre</h3>
+                                        <p id="apellidos_us" class="text-muted text-center">Apellidos</p>
                                     </div>
                                     <ul class="list-group list-group-unbordered mb-3">
                                         <li class="list-group-item">
                                             <b style="color:#0B7300">Edad</b>
-                                            <a id= "edad" href="" class="float-right">12</a>
+                                            <a id="edad" href="" class="float-right">12</a>
                                         </li>
                                         <li class="list-group-item">
                                             <b style="color:#0B7300">DNI</b>
-                                            <a id= "dni_us" href="" class="float-right">12345678</a>
+                                            <a id="dni_us" href="" class="float-right">12345678</a>
                                         </li>
                                         <li class="list-group-item">
                                             <b style="color:#0B7300">Tipo de usuario</b>
-                                            <span id= "us_tipo" class="float-right badge badge-primary">Jodido</span>
+                                            <span id="us_tipo" class="float-right badge badge-primary">Jodido</span>
                                         </li>
+                                        <button data-toggle="modal" data-target="#cambioContra" type="button" class="btn btn-block btn-outline-warning btn-sm">Cambiar contraseña</button>
                                     </ul>
                                 </div>
                             </div>
@@ -65,27 +115,27 @@ if ($_SESSION['us_tipo'] == 1) {
                                     <strong style="color:#0B7300">
                                         <i class="fas fa-phone mr-1"> Telefono</i>
                                     </strong>
-                                    <p id= "telefono_us" class="text-muted">999 98 78 77</p>
+                                    <p id="telefono_us" class="text-muted">999 98 78 77</p>
 
                                     <strong style="color:#0B7300">
-                                    <i class="fas fa-map-marker-alt mr-1"> Residencia</i>
+                                        <i class="fas fa-map-marker-alt mr-1"> Residencia</i>
                                     </strong>
-                                    <p id= "residencia_us"class="text-muted">Av zarzuela</p>
+                                    <p id="residencia_us" class="text-muted">Av zarzuela</p>
 
                                     <strong style="color:#0B7300">
                                         <i class="fas fa-at mr-1"> Correo</i>
                                     </strong>
-                                    <p id= "correo_us" class="text-muted">gggg@gmail.com</p>
+                                    <p id="correo_us" class="text-muted">gggg@gmail.com</p>
 
                                     <strong style="color:#0B7300">
                                         <i class="fas fa-user mr-1"> Sexo</i>
                                     </strong>
-                                    <p id= "sexo_us" class="text-muted">Bisexual</p>
-                                    
+                                    <p id="sexo_us" class="text-muted">Bisexual</p>
+
                                     <strong style="color:#0B7300">
                                         <i class="fas fa-pencil-alt mr-1"> Adicional</i>
                                     </strong>
-                                    <p id= "adicional_us" class="text-muted">Hola soy</p>
+                                    <p id="adicional_us" class="text-muted">Hola soy</p>
 
                                     <button class="edit btn btn-block bg-gradient-danger">Editar</button>
 
